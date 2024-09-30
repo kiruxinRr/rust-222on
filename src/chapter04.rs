e86c89492cc6e8be5a75ed9502c7402203b6c4d2
chapter03.rs <r.kiryuhin@e-u.edu.ua>
2024-09-30 21:16:15 +0300

    chapter04.rs

--- src/chapter04.rs
+++ src/chapter04.rs
@@ -201,6 +201,44 @@ mod tests {
 
 
 //4-4.3//
+#[test]
+fn test18() {
+    let v = {
+        let mut x = 1;
+        x += 2;
+        x
+    };
+
+    assert_eq!(v, 3);
+
+    println!("Success!");
+}
+
+#[test]
+fn test19() {
+    let v = {
+        let x = 3;
+        x
+    };
+
+    assert!(v == 3);
+
+    println!("Success!");
+}
+
+#[test]
+fn test20() {
+    let s = sum(1, 2);
+    assert_eq!(s, 3);
+
+    println!("Success!");
+}
+
+fn sum(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+
 
 
 
