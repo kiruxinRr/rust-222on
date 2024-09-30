2428e49706612155900ab18bb0321db429670a38
chapter03.rs <r.kiryuhin@e-u.edu.ua>
2024-09-30 18:05:10 +0300

    chapter04.rs

--- src/chapter04.rs
+++ src/chapter04.rs
@@ -128,6 +128,81 @@ fn test11() {
 }
 
 
+//4-4.2//
+
+
+
+use std::mem::size_of_val;
+
+#[test]
+fn test12() {
+    let c1 = 'a';
+    assert_eq!(size_of_val(&c1), 4);
+
+    let c2 = '中';
+    assert_eq!(size_of_val(&c2), 4);
+}
+
+
+#[test]
+fn test13() {
+    let c1 = '中';
+    print_char(c1);
+}
+
+fn print_char(c: char) {
+    println!("{}", c);
+}
+
+#[test]
+fn test14() {
+    let _f: bool = false;
+
+    let t = true;
+    if t {
+        println!("Success!");
+    }
+}
+#[test]
+fn test15() {
+    let f = false;
+    let t = false; // true && false завжди дорівнює false
+    assert_eq!(t, f);
+
+    println!("Success!");
+}
+
+
+#[test]
+fn test16() {
+    let _v: () = ();
+
+    let v = ();
+    assert_eq!(v, implicitly_ret_unit());
+
+    println!("Success!");
+}
+
+fn implicitly_ret_unit() {
+    println!("I will return a ()");
+}
+
+#[cfg(test)]
+mod tests {
+    use std::mem::size_of_val;
+
+    #[test]
+    fn test17() {
+        let unit: () = ();
+        assert!(size_of_val(&unit) == 0);
+    }
+}
+
+
+
+//4-4.3//
+
+
 
 
 
